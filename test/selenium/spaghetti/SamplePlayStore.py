6657358d188b4aa270694fda40be579a2ba18ad3
Joe Yasuta <s7601096@gmail.com>
2017-11-18 18:00:50 +0900

    ReViewの関係で改行対応

--- test/selenium/spaghetti/SamplePlayStore.py
+++ test/selenium/spaghetti/SamplePlayStore.py
@@ -17,7 +17,8 @@ class SamplePlayStoreTest(unittest.TestCase):
         #
         # self.driver = webdriver.Chrome(chrome_options=options, executable_path='/usr/local/bin/chromedriver')
 
-        self.driver = webdriver.Firefox(executable_path='/usr/local/bin/geckodriver')
+        self.driver = webdriver.Firefox(
+            executable_path='/usr/local/bin/geckodriver')
 
     def tearDown(self):
         self.driver.close()
@@ -27,9 +28,11 @@ class SamplePlayStoreTest(unittest.TestCase):
         driver.get("https://play.google.com/store")
 
         WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, ".id-corpus-nav-list :nth-child(2) a"))
+            EC.element_to_be_clickable(
+                By.CSS_SELECTOR, ".id-corpus-nav-list :nth-child(2) a")
         )
-        driver.find_element_by_css_selector(".id-corpus-nav-list :nth-child(2) a").click()
+        driver.find_element_by_css_selector(
+            ".id-corpus-nav-list :nth-child(2) a").click()
 
         WebDriverWait(driver, 10).until(
             EC.title_is("Google Play の Android アプリ")
@@ -38,14 +41,16 @@ class SamplePlayStoreTest(unittest.TestCase):
 
         driver.execute_script('window.scrollTo(0, 400);')
         element = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, ".id-card-list :nth-child(1) div a"))
+            EC.element_to_be_clickable(
+                By.CSS_SELECTOR, ".id-card-list :nth-child(1) div a")
         )
         element.click()
 
         WebDriverWait(driver, 10).until(
             EC.title_contains("Google Play の Android アプリ")
         )
-        assert "https://play.google.com/store/apps/details" in driver.current_url
+        assert "https://play.google.com/store/apps/details" \
+               in driver.current_url
 
 
 if __name__ == "__main__":
